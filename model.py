c62830f0f913afac7f78b4441c1d53d4f32ea51e
haris461 <158203393+haris461@users.noreply.github.com>
2025-03-01 14:08:53 +0500

    Create model.py

---
+++ model.py
@@ -0,0 +1,15 @@
+import os
+import requests
+
+MODEL_URL = "https://github.com/haris461/arabic_to_english-translator/releases/tag/4.46.3"
+MODEL_PATH = "nmt_model.pkl"
+
+# Check if the model file exists, if not, download it
+if not os.path.exists(MODEL_PATH):
+    with requests.get(MODEL_URL, stream=True) as r:
+        r.raise_for_status()
+        with open(MODEL_PATH, "wb") as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+    print("Model downloaded successfully!")
+
